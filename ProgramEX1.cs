80eb104a5a6b5ded8bcf0e7397b1b880ba31b02e
RaicaB <raicamartins97@gmail.com>
2026-04-13 19:40:44 -0300

    Add files via upload

---
+++ ProgramEX1.cs
@@ -0,0 +1,28 @@
+﻿
+public class Program
+
+{
+
+    public static void Main(string[] args)
+
+    {
+
+        Aluno aluno = new Aluno();
+
+
+
+        aluno.ReceberDados();
+
+        aluno.CalcularMedia();
+
+        aluno.ImprimirNotaFinal();
+
+        aluno.CalcularNotaFinal();
+
+
+
+        Console.ReadKey();
+
+    }
+
+}
